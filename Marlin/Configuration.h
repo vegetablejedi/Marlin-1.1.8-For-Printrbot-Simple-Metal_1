18037bd6e14354c23d45d14a2f8154aed83038bb
Hyun-Kook Yeo <darkjedi@cqtost.net>
2018-07-09 21:37:45 +0900

    Rollback of Z-probe offset limitation - Min: -20, Max: 20

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -762,8 +762,8 @@
 #define Z_CLEARANCE_BETWEEN_PROBES  3 // Z Clearance between probe points
 
 // For M851 give a range for adjusting the Z probe offset
-#define Z_PROBE_OFFSET_RANGE_MIN -1.6
-#define Z_PROBE_OFFSET_RANGE_MAX 0
+#define Z_PROBE_OFFSET_RANGE_MIN -20
+#define Z_PROBE_OFFSET_RANGE_MAX 20
 
 // Enable the M48 repeatability test to test probe accuracy
 //#define Z_MIN_PROBE_REPEATABILITY_TEST
